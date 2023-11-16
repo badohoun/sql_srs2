c072014c98a98a0d2fb5c14bed6eab9ccf6cc167
e-badohoun <e-badohoun@autodistribution.com>
2023-11-16 17:25:04 +0100

    add cleanup code with black and fixed yaml file
    :wq
    
    qq
    wq
    q

--- app.py
+++ app.py
@@ -1,51 +1,64 @@
+# pylint: disable=missing-module-docstring
 
-import streamlit as st
-import pandas as pd
-import duckdb as du
 import io
 
-csv = '''
+import duckdb as du
+import pandas as pd
+import streamlit as st
+
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
+"""
 
 
-csv2 = '''
+CSV2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
-'''
-beverages = pd.read_csv(io.StringIO(csv))
+"""
+beverages = pd.read_csv(io.StringIO(CSV))
 
-food_items = pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+answer_str = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-solution = du.sql(answer).df()
+solution_df = du.sql(answer_str).df()
 with st.sidebar:
     option = st.selectbox(
-
         "How would you like to be review?",
         ["Joins", "GroupBy", "Windows Functions"],
         index=None,
         placeholder="Select a theme...",
     )
-    st.write('You selected:', option)
-
+    st.write("You selected:", option)
 
 
 st.header("enter your code:")
-query = st.text_area(label="votre code SQL ici" , key="user_input")
+query = st.text_area(label="votre code SQL ici", key="user_input")
 
 if query:
     result = du.sql(query).df()
     st.dataframe(result)
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f" result has a {n_lines_difference} lines differences with the solution_df"
+        )
+
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -55,8 +68,8 @@ with tab2:
     st.write("table: food_items")
     st.dataframe(food_items)
     st.write("expected:")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 
 with tab3:
-    st.write(answer)
+    st.write(answer_str)
