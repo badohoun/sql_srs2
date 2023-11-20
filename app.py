b23618f7707fa5211d732ad32ea77d61d6c452a9
e-badohoun <e-badohoun@autodistribution.com>
2023-11-20 22:27:00 +0100

    comparaison with real answer

--- app.py
+++ app.py
@@ -26,12 +26,32 @@ with st.sidebar:
     exercise = con.execute(f"select * from memory_state where theme = '{theme}'").df()
     st.write(exercise)
 
+
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("enter your code:")
 
 query = st.text_area(label="votre code SQL ici", key="user_input")
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
+
+    try:
+        result=result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference !=0:
+        st.write(
+            f"result has a in {n_lines_difference} lines difference with the solution_df"
+        )
+
+
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -43,7 +63,7 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise_tables.loc[0,"exercise_name"]
+    exercise_name = exercise.loc[0,"exercise_name"]
     with open(f"answers/{exercise_name}.sql", "r") as f:
         answer = f.read()
     st.write(answer)
