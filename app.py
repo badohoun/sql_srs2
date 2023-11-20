1c4f5766d2b80560b67ede5a29951ac560943670
e-badohoun <e-badohoun@autodistribution.com>
2023-11-20 22:01:15 +0100

    use user input to display exercise specifics

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring
 
 import io
-
+import ast
 import duckdb as du
 import pandas as pd
 import streamlit as st
@@ -15,18 +15,39 @@ CROSS JOIN food_items
 con = du.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 #solution_df = du.sql(answer_str).df()
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "How would you like to be review?",
-        ["cross_joins", "GroupBy", "Windows Functions"],
+        ["cross_joins", "GroupBy", "window_functions"],
         index=None,
         placeholder="Select a theme...",
     )
-    st.write("You selected:", option)
+    st.write("You selected:", theme)
 
-    exercise  = con.execute("select * from memory_state where theme= '{theme}'").df()
+    exercise = con.execute(f"select * from memory_state where theme = '{theme}'").df()
     st.write(exercise)
-
 
 st.header("enter your code:")
 
 query = st.text_area(label="votre code SQL ici", key="user_input")
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+with tab3:
+    exercise_name = exercise_tables.loc[0,"exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+    st.write(answer)
+
+
+
+
